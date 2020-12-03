5c6893a6bbcac9d78ef18275874bb1098cec6cf5
yamamishunsuke <ys26247ys@gmail.com>
2020-12-03 15:41:25 +0900

    SQLITEを使って友達追加機能

--- app/src/main/java/to/msn/wings/demo/AddFriendFragment.java
+++ app/src/main/java/to/msn/wings/demo/AddFriendFragment.java
@@ -1,7 +1,7 @@
 package to.msn.wings.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+import android.content.ContentValues;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -13,15 +13,18 @@ import androidx.annotation.NonNull;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+import android.view.View.OnTouchListener;
 
 public class AddFriendFragment extends Fragment {
     private int cnt = 0;
     EditText addfriend;
+    private FriendDatabaseHelper helper = null;
+    private EditText txtName = null;
+    private TextView view;
 
     static AddFriendFragment newInstance(int count){
         // Fragemnt02 インスタンス生成
@@ -37,12 +40,20 @@ public class AddFriendFragment extends Fragment {
 
     // FragmentのViewを生成して返す
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container2,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container2, Bundle savedInstanceState) {
+
+        helper = new FriendDatabaseHelper(getActivity());
 
         return inflater.inflate(R.layout.fragment_addfriend,
                 container2, false);
+//
+//        view.setOnTouchListener(new View.OnTouchListener() {
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if(event.getAction() == MotionEvent.ACTION_MOVE){
+//                }
+//                return true;
+//            }
+//        });
     }
 
     @Override
@@ -62,15 +73,31 @@ public class AddFriendFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
-//        // BackStackで１つ戻す
-//        Button pop02 = view.findViewById(R.id.pop_02);
-//        pop02.setOnClickListener( v -> {
-//            FragmentManager fragmentManager = getParentFragmentManager();
-//            if(fragmentManager != null) {
-//                fragmentManager.popBackStack();
-//            }
-//        });
+
+        Button btnSave = view.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener( v -> {
+            try (SQLiteDatabase db = helper.getWritableDatabase()) {
+                ContentValues cv  = new ContentValues();
+                cv.put("name", txtName.getText().toString());
+                db.insert("friend", null, cv);
+//          重複した時の処理
+                db.insertWithOnConflict("books", null, cv, SQLiteDatabase.CONFLICT_REPLACE);
+                Toast.makeText(getActivity(), "データの登録に成功しました。", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+//    //  保存ボタンを押した時に呼び出されるメソッド
+//    public void save_onClick(View view) {
+//        try (SQLiteDatabase db = helper.getWritableDatabase()) {
+//            ContentValues cv  = new ContentValues();
+//            cv.put("name", txtName.getText().toString());
+//            db.insert("friend", null, cv);
+////          重複した時の処理
+//            db.insertWithOnConflict("books", null, cv, SQLiteDatabase.CONFLICT_REPLACE);
+//            Toast.makeText(getActivity(), "データの登録に成功しました。", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 }
 
 
